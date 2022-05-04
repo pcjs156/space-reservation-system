be975f6494f76103862b2a8dbccdd10c8e859bee
Yubin Park <itqkrdbqls1001@naver.com>
2022-05-05 02:00:24 +0900

    feat(users): blinking logo

--- static/commons/js/base.js
+++ static/commons/js/base.js
@@ -1,19 +1,39 @@
 const onLogoutButtonMouseOver = function () {
-    const logoutButton = document.getElementById('logoutButton');
+    const logoutButton = document.getElementById('logout-button');
     logoutButton.innerText = 'Seriously?';
 };
 
-const onLogoutButtonMouseLeave = function () {
-    const logoutButton = document.getElementById('logoutButton');
+const onLogoutButtonMouseOut = function () {
+    const logoutButton = document.getElementById('logout-button');
     logoutButton.innerText = 'Logout';
 };
 
 const onLoginButtonMouseOver = function () {
-    const loginButton = document.getElementById('loginButton');
+    const loginButton = document.getElementById('login-button');
     loginButton.innerText = 'Welcome!';
-}
+};
 
-const onLoginButtonMouseLeave = function () {
-    const loginButton = document.getElementById('loginButton');
+const onLoginButtonMouseOut = function () {
+    const loginButton = document.getElementById('login-button');
     loginButton.innerText = 'Login';
-}
+};
+
+const onLogoMouseOver = function () {
+    const logo = document.getElementById('header-central-logo');
+
+    const pathList = (new URL(logo.src)).pathname.split('/');
+    pathList.pop();
+    pathList.push('logo_transparent_darker.png');
+
+    logo.src = pathList.join('/');
+};
+
+const onLogoMouseOut = function () {
+    const logo = document.getElementById('header-central-logo');
+
+    const pathList = (new URL(logo.src)).pathname.split('/');
+    pathList.pop();
+    pathList.push('logo_transparent.png');
+
+    logo.src = pathList.join('/');
+};
