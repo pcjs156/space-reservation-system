41953c7eb2a6030fcf4aa9805211e02d5d97c15a
Yubin Park <itqkrdbqls1001@naver.com>
2022-05-10 21:58:54 +0900

    feat(common): Implement group invite code change

--- static/users/js/group_manage.js
+++ static/users/js/group_manage.js
@@ -1,3 +1,4 @@
+// 그룹 삭제 관련
 const groupDeleteBtn = document.getElementById('groupDeleteBtn');
 
 let groupDeleteModal = document.getElementById('groupDeleteModal');
@@ -14,4 +15,31 @@ groupDeleteBtn.addEventListener('click', () => {
 
 groupNameInput.addEventListener('input', () => {
     groupDeleteConfirmBtn.disabled = groupNameInput.value !== groupName;
+});
+
+// 그룹 초대 코드 변경 관련
+const groupInviteCodeInput = document.getElementById('groupInviteCodeInput');
+
+const reissueInviteCodeBtn = document.getElementById('reissueInviteCodeBtn');
+reissueInviteCodeBtn.addEventListener('click', () => {
+    const requestURL = reissueInviteCodeBtn.getAttribute('requestURL');
+    const httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+        alert('Failed to request: code 01');
+    }
+
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                const result = JSON.parse(httpRequest.responseText);
+                groupInviteCodeInput.setAttribute('value', result.new_invite_code);
+            } else {
+                alert('Failed to request: code 02');
+            }
+        }
+    };
+
+    httpRequest.open('GET', requestURL);
+    httpRequest.send();
 });
